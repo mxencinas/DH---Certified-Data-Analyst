51bbdc832b084c89061311c6f89218c2091194d8
Maximiliano <mxmencinas@gmail.com>
2024-11-19 21:58:37 -0300

    Estructuras en Python

---
+++ Programacion en Python/01_Estructuras.py
@@ -0,0 +1,24 @@
+# SECUENCIAS
+
+una_lista = [1, 2, 4, 8] # list - ordenada, mutable
+
+una_tupla = (1, 2, 4, 8) # tuple - ordenada, inmutable
+
+una_cadena = "Esto es una cadena de texto" # str- ordenada, inmutable | secuencia de caracteres
+
+un_rango = range(0, 10) # range - ordenada, inmutable | secuencia de numeros enteros
+
+
+# MAPPING TYPE
+un_diccionario = {
+    "nombre": "Max", 
+    "edad": 17
+    } # dict - coleccion no ordenada de pares clave-valor
+
+
+# CONJUNTOS
+un_set = {0, 1, 2, 3} # set - coleccion no ordenada, mutable de elementos unicos
+un_frozenset = frozenset([0,1,2,5]) # frozenset - coleccion no ordenada, inmutable de elementos unicos
+
+# TIPO NULO
+un_none = None # None - representa la ausencia de valor o la No definicion 
